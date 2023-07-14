3df7a23e0b8fbd5646caf29a482e6e2e971c7b17
DevaWinner <winneraaniekan@gmail.com>
2023-07-14 14:45:55 +0100

    Add calculator file and create result component

---
+++ src/components/Calculator.js
@@ -0,0 +1,10 @@
+import './stylesheet/calculator.css';
+
+function ResultLine() {
+  return (
+    <div className="line result-line">
+      <p className="result-line-text">0</p>
+    </div>
+  );
+}
+

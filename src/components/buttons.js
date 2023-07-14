86534dc484b079147be0382fef8eb265525dcc47
DevaWinner <winneraaniekan@gmail.com>
2023-07-15 00:35:53 +0100

    Remove Result from buttons file

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,65 +1,80 @@
-function ResultLine() {
-  return (
-    <div className="line result-line">
-      <p className="result-line-text">0</p>
-    </div>
-  );
-}
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function FirstLine() {
+function FirstLine({ handleClick }) {
   return (
     <div className="line first-line">
-      <button className="first-line-button" type="button">AC</button>
-      <button className="first-line-button" type="button">+/-</button>
-      <button className="first-line-button" type="button">%</button>
-      <button className="first-line-button last" type="button">+</button>
+      <button className="first-line-button" type="button" onClick={handleClick}>AC</button>
+      <button className="first-line-button" type="button" onClick={handleClick}>+/-</button>
+      <button className="first-line-button" type="button" onClick={handleClick}>%</button>
+      <button className="first-line-button last" type="button" onClick={handleClick}>÷</button>
     </div>
   );
 }
 
-function SecondLine() {
+FirstLine.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+function SecondLine({ handleClick }) {
   return (
     <div className="line second-line">
-      <button className="second-line-button" type="button">7</button>
-      <button className="second-line-button" type="button">8</button>
-      <button className="second-line-button" type="button">9</button>
-      <button className="second-line-button last" type="button">x</button>
+      <button className="second-line-button" type="button" onClick={handleClick}>7</button>
+      <button className="second-line-button" type="button" onClick={handleClick}>8</button>
+      <button className="second-line-button" type="button" onClick={handleClick}>9</button>
+      <button className="second-line-button last" type="button" onClick={handleClick}>x</button>
     </div>
   );
 }
 
-function ThirdLine() {
+SecondLine.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+function ThirdLine({ handleClick }) {
   return (
     <div className="line third-line">
-      <button className="third-line-button" type="button">4</button>
-      <button className="third-line-button" type="button">5</button>
-      <button className="third-line-button" type="button">6</button>
-      <button className="third-line-button last" type="button">-</button>
+      <button className="third-line-button" type="button" onClick={handleClick}>4</button>
+      <button className="third-line-button" type="button" onClick={handleClick}>5</button>
+      <button className="third-line-button" type="button" onClick={handleClick}>6</button>
+      <button className="third-line-button last" type="button" onClick={handleClick}>-</button>
     </div>
   );
 }
 
-function FourthLine() {
+ThirdLine.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+function FourthLine({ handleClick }) {
   return (
     <div className="line fourth-line">
-      <button className="fourth-line-button" type="button">1</button>
-      <button className="fourth-line-button" type="button">2</button>
-      <button className="fourth-line-button" type="button">3</button>
-      <button className="fourth-line-button last" type="button">+</button>
+      <button className="fourth-line-button" type="button" onClick={handleClick}>1</button>
+      <button className="fourth-line-button" type="button" onClick={handleClick}>2</button>
+      <button className="fourth-line-button" type="button" onClick={handleClick}>3</button>
+      <button className="fourth-line-button last" type="button" onClick={handleClick}>+</button>
     </div>
   );
 }
 
-function FifthLine() {
+FourthLine.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+function FifthLine({ handleClick }) {
   return (
     <div className="line fifth-line">
-      <button className="fifth-line-button first" type="button">0</button>
-      <button className="fifth-line-button" type="button">.</button>
-      <button className="fifth-line-button last" type="button">=</button>
+      <button className="fifth-line-button first" type="button" onClick={handleClick}>0</button>
+      <button className="fifth-line-button" type="button" onClick={handleClick}>.</button>
+      <button className="fifth-line-button last" type="button" onClick={handleClick}>=</button>
     </div>
   );
 }
 
+FifthLine.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
 export {
-  ResultLine, FirstLine, SecondLine, ThirdLine, FourthLine, FifthLine,
+  FirstLine, SecondLine, ThirdLine, FourthLine, FifthLine,
 };

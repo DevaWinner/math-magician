93a2f45cbea9303e3b0cadf17f63f655aa9eaae3
DevaWinner <winneraaniekan@gmail.com>
2023-07-15 00:36:35 +0100

    Add objects to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,30 @@
+import React, { useState } from 'react';
 import './stylesheet/calculator.css';
 import {
-  ResultLine, FirstLine, SecondLine, ThirdLine, FourthLine, FifthLine,
+  FirstLine, SecondLine, ThirdLine, FourthLine, FifthLine,
 } from './buttons';
+import ResultLine from './ResultLine';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [obj, setObject] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    setObject(calculate(obj, e.target.innerHTML));
+  };
+
   return (
     <div className="calculator">
-      <ResultLine />
-      <FirstLine />
-      <SecondLine />
-      <ThirdLine />
-      <FourthLine />
-      <FifthLine />
+      <ResultLine total={obj.total} operation={obj.operation} next={obj.next} />
+      <FirstLine handleClick={handleClick} />
+      <SecondLine handleClick={handleClick} />
+      <ThirdLine handleClick={handleClick} />
+      <FourthLine handleClick={handleClick} />
+      <FifthLine handleClick={handleClick} />
     </div>
   );
 }

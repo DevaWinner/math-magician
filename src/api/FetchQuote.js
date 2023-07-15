4d824c6d80d49499fdd29416abe1344246c27b71
DevaWinner <winneraaniekan@gmail.com>
2023-07-15 08:53:03 +0100

    Add fetchdata function

--- src/api/FetchQuote.js
+++ src/api/FetchQuote.js
@@ -4,3 +4,31 @@ function FetchQuote() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        'https://api.api-ninjas.com/v1/quotes?category=success',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Api-Key': 'h51HAduSmK3j8nDhJsGZ7g==UOrxjLtOdw50QXKB',
+          },
+        },
+      );
+      const responseData = await response.json();
+      if (response.ok) {
+        setIsLoading(false);
+        setError(false);
+        setData(responseData[0]);
+      } else {
+        setIsLoading(false);
+        setError(responseData);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError(true);
+    }
+  };
